129cef9c9200221d50bebb26e53c222bb4992ac5
Marlon2025c <bastien.lecour2025@gmail>
2023-12-15 16:18:41 +0100

    🐛

---
+++ [addons]/[Voiture]/[carsounds]/ta178amgb/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+files {
+  'audioconfig/*.dat151.rel',
+  'audioconfig/*.dat54.rel',
+  'sfx/**/*.awc'
+}
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/ta178amgb_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/ta178amgb_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_ta178amgb'
